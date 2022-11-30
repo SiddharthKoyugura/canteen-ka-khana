5dbd3412a9455172e424c7b4f65e654afd4975e6
SiddharthKoyugura <koyugurasiddhardha@gmail.com>
2022-11-30 14:18:43 +0530

    routing completed

--- main.py
+++ main.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+ 
 if __name__ == "__main__":
     app.run(host="localhost", port=3000, debug=True)
